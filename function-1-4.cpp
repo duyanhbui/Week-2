c094f8a49b6a610508827c48248fee8b8a1ef98f
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-08-04 10:40:25 +0930

    haizzz

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
-void print_summed(int array1[3][3],int array2[3][3]){
+void print_scaled(int array[3][3],int scale){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                array1[i][j] += array2[i][j];
+                array[i][j] *= scale;
                 if (j < 2){
-                    std :: cout << array1[i][j] << " ";
+                    std :: cout << array[i][j] << " ";
                 }else{
-                    std :: cout << array1[i][j];
+                    std :: cout << array[i][j];
                 }
-                
-        }
+            }
         std :: cout << "\n";
-    }
-                
             
-            
+        }
 }
